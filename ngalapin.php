6b5b6a906bf80116bb49e5d6236959f486199647
parbada <goodtogel207@gmail.com>
2025-05-17 13:52:53 +0700

    Update ngalapin.php

--- ngalapin.php
+++ ngalapin.php
@@ -1,2 +1,1 @@
-<h1><center>Rabbanaa dholamna anfusana wa illaa taghfir lanaa wa tarhamnaa lanakunanna minal khoosirin</center></h1>
-<h2><center>"ARTINYA : Ya Tuhan kami, kami telah menzalimi diri kami sendiri. Jika Engkau tidak mengampuni kami dan tidak merahmati kami, niscaya kami termasuk orang-orang yang rugi."</center></h2>
+<h1><center>VELOCITY + POSE KIYOWO SEKALI BANG TING PLISS</center></h1>
