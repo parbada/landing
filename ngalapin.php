00785e35c070d51a32d3f79d786ab9865faf0f31
parbada <goodtogel207@gmail.com>
2025-05-17 13:50:38 +0700

    Update ngalapin.php

--- ngalapin.php
+++ ngalapin.php
@@ -1,1 +1,2 @@
-<h1><center>BANYAK-BANYAK BELAJAR LAGI YA DEK KU</center></h1>
+<h1><center>Rabbanaa dholamna anfusana wa illaa taghfir lanaa wa tarhamnaa lanakunanna minal khoosirin</center></h1>
+<h2><center>"ARTINYA : Ya Tuhan kami, kami telah menzalimi diri kami sendiri. Jika Engkau tidak mengampuni kami dan tidak merahmati kami, niscaya kami termasuk orang-orang yang rugi."</center></h2>
